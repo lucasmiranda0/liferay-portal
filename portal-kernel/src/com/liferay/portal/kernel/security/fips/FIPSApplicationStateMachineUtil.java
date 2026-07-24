1e9f6934c72adee417d54557458070eb7e6a436e
Jorge García Jiménez <jorge.garcia@liferay.com>
2026-07-24 10:25:22 +0200

    LPD-93274 Drive the FIPS application state machine at boot

---
+++ portal-kernel/src/com/liferay/portal/kernel/security/fips/FIPSApplicationStateMachineUtil.java
@@ -0,0 +1,39 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.kernel.security.fips;
+
+/**
+ * @author Jorge García Jiménez
+ */
+public class FIPSApplicationStateMachineUtil {
+
+	public static FIPSApplicationState getFIPSApplicationState() {
+		return _fipsApplicationStateMachine.getFIPSApplicationState();
+	}
+
+	public static void selfTest(Runnable runnable) {
+		transition(FIPSApplicationState.SELF_TEST);
+
+		try {
+			runnable.run();
+		}
+		catch (Throwable throwable) {
+			transition(FIPSApplicationState.ERROR);
+
+			throw throwable;
+		}
+
+		transition(FIPSApplicationState.OPERATIONAL);
+	}
+
+	public static void transition(FIPSApplicationState fipsApplicationState) {
+		_fipsApplicationStateMachine.transition(fipsApplicationState);
+	}
+
+	private static final FIPSApplicationStateMachine
+		_fipsApplicationStateMachine = new FIPSApplicationStateMachine();
+
+}
